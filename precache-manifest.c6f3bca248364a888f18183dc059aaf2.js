5e5591dca89b5ee7e4b9ad422fb36a1e3cd1b656
raxemmohamed <raxemmohamed@gmail.com>
2019-09-17 19:33:41 -0500

    Updates

---
+++ precache-manifest.c6f3bca248364a888f18183dc059aaf2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bddb7d14d71ffa22ba2b2a2a0e8f52ee",
+    "url": "/reactgame/index.html"
+  },
+  {
+    "revision": "3c395064b5f33f8f61cb",
+    "url": "/reactgame/static/css/main.dc098c5e.chunk.css"
+  },
+  {
+    "revision": "a66641424edfe796aae0",
+    "url": "/reactgame/static/js/2.92bbc6ac.chunk.js"
+  },
+  {
+    "revision": "3c395064b5f33f8f61cb",
+    "url": "/reactgame/static/js/main.2b45e5d3.chunk.js"
+  },
+  {
+    "revision": "1721ca2de24c4eab4e5c",
+    "url": "/reactgame/static/js/runtime~main.446b0c0d.js"
+  }
+]);
